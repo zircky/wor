dc24364a1d9cbba800ab38da2f4d457c78e2ae6f
ziran <gfgfgfggf02@gmail.com>
2023-12-14 22:58:11 +0300

    graphql

---
+++ src/graphql/graphql.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+
+@Module({
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    playground: true,
+    autoSchemaFile: './src/schema.gql'
+  })]
+})
+export class GraphqlModule {}
